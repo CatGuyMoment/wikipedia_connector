1101d23e16c6f1d1bd4dbccbcda6748ffb95c9cd
CatGuyMoment <amirchik57@outlook.com>
2025-02-20 22:46:22 +0100

    idk anymore

--- ai_heuristic.py
+++ ai_heuristic.py
@@ -6,7 +6,7 @@ from transformers import logging
 logging.set_verbosity_error()
 
 
-device = torch.device("cuda")
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') #I HATE MACBOOKS I HATE MACBOOKS
 
 # Use a pretrained BERT model and its tokenizer
 tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
@@ -20,8 +20,8 @@ embedding_layer = model.get_input_embeddings()
 def embed_word(word):
     # Tokenize the word without adding special tokens ([CLS], [SEP])
     token_ids = tokenizer.encode(word, add_special_tokens=False)
-    token_tensor = torch.tensor(token_ids).to(device)
-    
+    token_tensor = torch.tensor(token_ids)
+    print(word)
     # Retrieve token embeddings from the embedding layer
     token_embeddings = embedding_layer(token_tensor)
     
@@ -29,11 +29,9 @@ def embed_word(word):
     word_embedding = torch.mean(token_embeddings, dim=0)
     return word_embedding
 
-def test_word_similarity(word1, word2):
+def score_word_similarity(word1, word2):
     embed1 = embed_word(word1)
     embed2 = embed_word(word2)
     # return torch.norm(embed1 - embed2)
     return F.cosine_similarity(embed1, embed2, dim=-1).item()
     
-
-print(test_word_similarity('hi everyone','Hello there people'))
---
+++ explore_cache_ai.py
@@ -0,0 +1,153 @@
+import requests
+import re
+import sqlite3
+
+from tqdm import tqdm
+from tqdm.asyncio import tqdm_asyncio
+
+import time
+import aiohttp
+import asyncio
+
+from ai_heuristic import score_word_similarity
+
+from urllib.parse import quote as encode_url
+
+
+clientTimeout = aiohttp.ClientTimeout(total=60*60*60) #timeout of 1 hour
+
+limiter = asyncio.Semaphore(100)
+
+connection = sqlite3.connect('cache.db')
+cursor = connection.cursor()
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS pages
+                   (
+                    name TEXT PRIMARY KEY,
+                    links TEXT
+                   )''')
+
+
+regex = '<a href="\/wiki\/.{1,50}" title=".{1,50}">.{1,50}<\/a>'
+
+connection.commit()
+
+session = requests.Session()
+
+# session.trust_env = True #FUCK FLORIMONT
+# session.verify = False
+
+
+def harvest_urls(wiki_page,path):
+    #cache
+    
+    cmd = cursor.execute(f'SELECT links from pages where name= "{wiki_page}" ') #bro if i get sql injected by a FUCKING WIKIPEDIA ARTICLE :sob:
+    response = cmd.fetchone()
+    if response:
+        res = response[0].split('::;')
+        parsed_response = []
+        
+        for split_response in res:
+            splitter = split_response.split('@@')
+            url = splitter[0]
+
+            urllabel = '[UNKNOWN TITLE]'
+            if len(splitter) == 2:
+                urllabel = splitter[1]
+
+            newpath = path + [ [wiki_page,urllabel ] ]
+            parsed_response.append([url,newpath])
+
+        return parsed_response
+    else:
+        return []
+
+local_cache = {
+    'Main_Page' : True, #anticheat,
+    '' : True
+
+}
+def harvest_data(start_page,path,end_page):
+    global local_cache
+    if start_page.count(':') != 0:
+        return False,[] #NO CHEATING!!!
+
+    if local_cache.get(start_page):
+        return False,[] #prune because we already scanned it
+    
+    data = harvest_urls(start_page,path)
+
+    for page_name, path in data:
+        if page_name == end_page:
+            return page_name, path
+        
+    local_cache[start_page] = True
+    return False, data
+
+def hunt(start_page,end_page):
+
+    data = []
+    
+    heuristic_scores = []
+
+    recursions = 0
+
+    response = None
+    data = [[start_page,[]]]
+    while not response:
+
+        print('checking values:')
+
+        if len(data) == 0:
+            print('Nothing stored in database.')
+            response = []
+
+        temp_data = []
+        print('harvesting page data..')
+
+        if recursions >= 3:
+            recursions = 0
+            heuristic_scores.sort(reverse=True,key = lambda data: data[1])
+            #ai optimisation: it picks the best path to route on based on what it already explored.
+            print(heuristic_scores[0],'score')
+            data = [[heuristic_scores[0][0],heuristic_scores[0][2]]]
+            heuristic_scores = []
+            
+        for page_name, path in tqdm(data):
+
+            #ai scoring
+            heuristic_scores.append([page_name,score_word_similarity(page_name,end_page),path])
+
+            match_found, new_data = harvest_data(page_name,path,end_page)
+            
+            if match_found:
+                print('match found!')
+                return new_data
+            
+            temp_data += new_data
+
+        recursions += 1
+
+        print('Nothing found, searching deeper...')
+        if len(data) == 0:
+            print('nothing found in the database :c')
+            return None
+        
+        data = temp_data
+
+        
+
+    return response
+        
+
+        
+
+            
+
+#NASA -> Lie
+def main():
+    start = time.time()
+    print(hunt('Sigmund_Freud','Institut_Florimont'))
+    print(time.time() - start)
+
+main()
